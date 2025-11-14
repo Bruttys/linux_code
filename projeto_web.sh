63ad200faeb86bc46c8b7406d9aa7f64357c9d27
Bruttys <gusferbar@gmail.com>
2025-11-14 16:17:40 -0300

    Primeira fonte

--- projeto_web.sh
+++ projeto_web.sh
@@ -1,56 +1,69 @@
 #!/bin/bash
-# ------------------------------------------------------------
-# Script de Automação: Instalação de Servidor Web Apache
-# Autor: [Seu Nome]
-# Descrição:
-#   - Interage com o usuário pedindo confirmação
-#   - Instala automaticamente o Apache
-#   - Baixa um template HTML da internet
-#   - Publica o template como página inicial do servidor
-# ------------------------------------------------------------
 
-# Função para exibir mensagens formatadas
-function info() {
-    echo -e "\n[INFO] $1\n"
-}
+# =============================================================================
+# Script de Automação - Deploy de Site (Debian/Ubuntu)
+#
+# Objetivo: Instalar e configurar um servidor web Apache com um
+#           template HTML baixado da internet.
+# =============================================================================
 
-# 1. Confirmação do usuário
-echo "Este script irá instalar e configurar um servidor WEB (Apache)."
-read -p "Deseja continuar? (s/n): " resposta
+# --- 1. Verificação de superusuário ---
+if [[ $EUID -ne 0 ]]; then
+   echo "Atenção: Este script precisa ser executado como root."
+   echo "Exemplo: sudo ./deploy_site.sh"
+   exit 1
+fi
 
-if [[ "$resposta" != "s" && "$resposta" != "S" ]]; then
-    echo "Instalação cancelada pelo usuário."
+# --- 2. Interação com o usuário ---
+echo "===================================================="
+echo " Script de Deploy Automático de Servidor Web Apache "
+echo "===================================================="
+echo "Este processo irá instalar o Apache e publicar um site."
+read -p "Deseja continuar? [s/n]: " decisao
+
+if [[ "$decisao" != "s" && "$decisao" != "S" ]]; then
+    echo "Execução cancelada pelo usuário."
+    exit 0
+fi
+
+# --- 3. Instalação direta no Debian ---
+echo "Atualizando lista de pacotes..."
+apt update -y
+
+echo "Instalando pacotes necessários (apache2, wget, unzip)..."
+apt install -y apache2 wget unzip
+
+# --- 4. Configuração do serviço Apache ---
+echo "Ativando e iniciando o serviço apache2..."
+systemctl enable --now apache2
+
+# --- 5. Download e publicação do template ---
+diretorio_web="/var/www/html"
+url_arquivo="https://templatemo.com/tm-zip-files-2020/templatemo_600_prism_flux.zip"
+arquivo_zip="/tmp/site.zip"
+pasta_interna="templatemo_600_prism_flux"
+
+echo "Baixando template de $url_arquivo..."
+wget -O $arquivo_zip $url_arquivo
+
+if [ $? -ne 0 ]; then
+    echo "Erro: Falha ao baixar o template."
     exit 1
 fi
 
-# 2. Atualizando pacotes do sistema
-info "Atualizando lista de pacotes..."
-sudo apt update -y
+echo "Publicando site em $diretorio_web..."
+rm -rf ${diretorio_web}/*
+unzip $arquivo_zip -d $diretorio_web/
 
-# 3. Instalando o Apache
-info "Instalando o servidor Apache..."
-sudo apt install apache2 -y
+mv ${diretorio_web}/${pasta_interna}/* ${diretorio_web}/
+rmdir ${diretorio_web}/${pasta_interna}
+rm $arquivo_zip
 
-# 4. Habilitando e iniciando o serviço
-info "Habilitando e iniciando o serviço Apache..."
-sudo systemctl enable apache2
-sudo systemctl start apache2
+# --- 6. Conclusão ---
+echo "===================================================="
+echo "--- Deploy concluído com sucesso! ---"
+echo "O Apache está rodando e o site foi publicado."
+echo "Acesse pelo navegador: http://localhost ou http://SEU_IP"
+echo "===================================================="
 
-# 5. Baixando template HTML da internet
-info "Baixando template HTML..."
-# Exemplo: usando um template simples do GitHub
-TEMPLATE_URL="https://raw.githubusercontent.com/BlackrockDigital/startbootstrap-creative/master/index.html"
-wget -O /tmp/index.html $TEMPLATE_URL
-
-# 6. Publicando o template no diretório padrão do Apache
-info "Publicando template no diretório padrão do Apache..."
-sudo cp /tmp/index.html /var/www/html/index.html
-
-# 7. Ajustando permissões
-info "Ajustando permissões..."
-sudo chown www-data:www-data /var/www/html/index.html
-sudo chmod 644 /var/www/html/index.html
-
-# 8. Finalização
-info "Instalação concluída!"
-echo "Abra o navegador e acesse: http://localhost ou http://SEU_IP"
+exit 0
